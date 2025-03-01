c184352a800395c6800cf68f988f99405c095ad7
Paulo Henrique <ph_lima18@hotmail.com>
2025-03-01 19:24:51 -0300

    Delete ELT directory

--- ELT/SqlAthena.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS dados_fakes (
-    Nome STRING,
-    Sobrenome STRING,
-    Data_Nascimento STRING,
-    Salario DOUBLE
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
-LOCATION 's3://meu-bucket-s3/dados_fakes.csv';
